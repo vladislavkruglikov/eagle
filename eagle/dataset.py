ca6b1fd9ffc9254d8bf76075aed7b7e34a223cb1
Vladislav Kruglikov <v.kruglikov@tbank.ru>
2025-06-11 09:06:19 +0000

    Add uniform noise to hidden states

--- eagle/dataset.py
+++ eagle/dataset.py
@@ -4,11 +4,19 @@ import pathlib
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_path: pathlib.Path, max_model_len: int):
+    def __init__(
+        self, 
+        dataset_path: pathlib.Path, 
+        max_model_len: int, 
+        transform_uniform_low: float, 
+        transformer_uniform_high: float
+    ):
         super().__init__()
         self._dataset_path = dataset_path
         self._dataset_size = len(os.listdir(dataset_path))
         self._max_model_len = max_model_len
+        self._transform_uniform_low = transform_uniform_low
+        self._transformer_uniform_high = transformer_uniform_high
 
     def __len__(self) -> int:
         return self._dataset_size
@@ -16,6 +24,7 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, index: int):
         data = torch.load(f"{self._dataset_path}/{index}.ckpt", map_location="cpu", weights_only=True)
         data["hidden_state"] = data["hidden_state"][:self._max_model_len]
+        data["hidden_state"] = self._apply_noise_to_hidden_state(hidden_state=data["hidden_state"])
         data["input_ids"] = data["input_ids"][:self._max_model_len]
         data["loss_mask"] = data["loss_mask"][:self._max_model_len]
         data["attention_mask"] = torch.ones(data["loss_mask"].shape[-1])
@@ -25,6 +34,11 @@ class Dataset(torch.utils.data.Dataset):
         # as next token
         data["target"] = torch.cat((data["hidden_state"][1:], torch.zeros(1, data["hidden_state"].shape[1])), dim=0)
         return data
+
+    def _apply_noise_to_hidden_state(self, hidden_state: torch.FloatTensor) -> None:
+        noise = torch.rand_like(hidden_state) * (self._transformer_uniform_high - self._transform_uniform_low) + self._transform_uniform_low
+        noisy_tensor = hidden_state + noise
+        return noisy_tensor
         
 
 if __name__ == "__main__":
